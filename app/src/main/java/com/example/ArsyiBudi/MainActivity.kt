532792c229fd8bf8eceed37284ad68c979e55743
ArsyiBudi <arsyibudi5@gmail.com>
2024-01-25 14:17:06 +0700

    fix: bug in activity main

--- app/src/main/java/com/example/ArsyiBudi/MainActivity.kt
+++ app/src/main/java/com/example/ArsyiBudi/MainActivity.kt
@@ -1,23 +1,16 @@
-package me.projeckandro.arsyi_recycler
+package com.example.ArsyiBudi
 
 import android.app.Activity
 import android.content.Intent
-import android.net.Uri
-import androidx.activity.result.contract.ActivityResultContracts
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-
-//import me.projeckandro.arsyi_recycler.model.ItemData
-//import me.projeckandro.arsyi_recycler.view.ItemAdapter
-import android.os.Build
-import android.widget.ImageView
-import com.example.ArsyiBudi.AddItemActivity
 import com.example.ArsyiBudi.ItemAdapter
 import com.example.ArsyiBudi.ItemData
-import com.example.ArsyiBudi.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,46 +19,64 @@ class MainActivity : AppCompatActivity() {
     lateinit var itemList: ArrayList<ItemData>
     lateinit var itemAdapter: ItemAdapter
 
-    private var imageUri: Uri? = null // Tambahkan deklarasi imageUri
-
     private val addResultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result ->
             val data: Intent? = result.data
-            val imageView: ImageView = findViewById(R.id.image_view)
-            if (result.resultCode == Activity.RESULT_OK && data != null) {
-                imageUri = data.data
-                imageView.setImageURI(imageUri)
+            if (result.resultCode == Activity.RESULT_OK) {
+                val newItem = if (Build.VERSION.SDK_INT >=
+                    Build.VERSION_CODES.TIRAMISU) {
+                        data?.getParcelableExtra("Item", ItemData::class.java)
+                    } else {
+                        @Suppress("DEPRECATION") data?.getParcelableExtra("item")
+                    }
+                    newItem?.let {
+                        itemList.add(it)
+                        itemAdapter.notifyItemInserted(itemList.size-1) // Update RecyclerView
+                }
             }
         }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         itemList = ArrayList()
+
         addBtn = findViewById(R.id.addingBtn)
         recycle = findViewById(R.id.mRecycler)
+
         itemAdapter = ItemAdapter(this, itemList)
+
         recycle.layoutManager = LinearLayoutManager(this)
         recycle.adapter = itemAdapter
+
         addBtn.setOnClickListener {
             val intent = Intent(this, AddItemActivity::class.java)
             addResultLauncher.launch(intent)
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?)
+    {
         super.onActivityResult(requestCode, resultCode, data)
+
         if (resultCode == Activity.RESULT_OK && requestCode == 2) {
             val newItem = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                data.getParcelableExtra<ItemData>("item")
+                data?.getParcelableExtra<ItemData>("item", ItemData::class.java)
             } else {
-                data.getParcelableExtra<ItemData>("item")
+                @Suppress("DEPRECATION") data?.getParcelableExtra("item")
             }
-            val position = data.getIntExtra("position", 0)
-            itemList[position] = newItem
-            itemAdapter.notifyItemChanged(position)
+            val position = data?.getIntExtra("position", 0)
+            newItem?.let {
+                itemList[position!!].title = it.title
+                itemList[position].subtitle = it.subtitle
+                itemList[position].desc = it.desc
+                itemList[position].img = it.img
+                itemAdapter.notifyDataSetChanged()      // Update RecyclerView
+                }
+            }
         }
     }
-}
 
